99eb74940ba7a8339e7d6bbaeb8ce2a6e816c2fa
Kostya <kostia1234455@gmail.com>
2021-04-07 23:08:13 +0300

    With connection(3)

--- configurations/settings.py
+++ configurations/settings.py
@@ -143,8 +143,8 @@ REST_FRAMEWORK = {
     ),
 }
 
-CORS_ORIGIN_ALLOW_ALL = True # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
-CORS_ALLOW_CREDENTIALS = True
+# CORS_ORIGIN_ALLOW_ALL = True # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
+# CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:8080',
     'http://localhost:8000',
